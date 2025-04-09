8b9c36a735ecbb3babdebcd7b6852703c667b769
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-09 10:53:28 +0530

    Implemented Kafka

--- src/main/java/dev/aman/e_commerce_email_service/Consumer/SendEmailConsumer.java
+++ src/main/java/dev/aman/e_commerce_email_service/Consumer/SendEmailConsumer.java
@@ -3,7 +3,15 @@ package dev.aman.e_commerce_email_service.Consumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.aman.e_commerce_email_service.DTOs.SendEmailDTOs;
+import dev.aman.e_commerce_email_service.Utils.EmailUtil;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
 
 @Service
 public class SendEmailConsumer {
@@ -14,6 +22,7 @@ public class SendEmailConsumer {
     }
 
     //Converting our Message to object
+    @KafkaListener(topics = "sendEmail", groupId = "emailService")
     public void handleSendEmailEvent(String message) throws JsonProcessingException {
 
         SendEmailDTOs sendEmailDTOs = objectMapper.readValue(message,
@@ -22,5 +31,24 @@ public class SendEmailConsumer {
         String to = sendEmailDTOs.getTo();
         String subject =sendEmailDTOs.getSubject();
         String body = sendEmailDTOs.getBody();
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("sharma.aman.a30@gmail.com"
+                        , "bmmtovxclofdgxvq");
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        EmailUtil.sendEmail(session, to,subject, body);
+
     }
 }
